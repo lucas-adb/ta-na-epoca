6b81a1d0255da3375058fea587bc0e3515ac86a8
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-03-17 15:43:16 -0300

    style: adds hove effect to links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,16 +6,16 @@ export default function Header() {
       <nav className="mx-auto max-w-screen-xl flex justify-between px-4 sm:px-6 lg:px-8 items-center h-16">
         <Link
           href="/"
-          className="font-sans font-extrabold text-green-900 text-2xl"
+          className="font-sans font-extrabold text-green-900 text-2xl transition-transform hover:-translate-y-1"
         >
           Tá na época?
         </Link>
 
         <div className="flex gap-4">
-          <Link href="/calendario" className="hover:text-green-900">
+          <Link href="/calendario" className="hover:text-green-900 transition-transform hover:-translate-y-1">
             Calendário
           </Link>
-          <Link href="/sobre" className="hover:text-green-900">
+          <Link href="/sobre" className="hover:text-green-900 transition-transform hover:-translate-y-1">
             Sobre
           </Link>
         </div>
