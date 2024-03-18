6536a1bbb68ee9fa372ead10f44786a3e248f32b
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-03-18 15:26:53 -0300

    refactor: adds FoodItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
 import Link from "next/link";
 import SimpleFadeIn from "@/components/framer/SimpleFadeIn";
 import { SimpleVariant } from "@/components/framer/SimpleVariant";
+import { filterFoodsByCurrentMonth } from "./utils/utils";
+import { fruitsAndVegetables } from "@/data/fruitsAndVegetables";
 
 export default function Home() {
+  const filteredFoods = filterFoodsByCurrentMonth(fruitsAndVegetables);
+
   return (
     <main className="flex-grow max-w-4xl mx-auto flex flex-col lg:justify-center">
-      <section className="flex flex-col items-center gap-8">
-        <SimpleFadeIn>
+      <SimpleFadeIn>
+        <section className="flex flex-col items-center gap-8">
           <h1 className="font-serif text-5xl lg:text-8xl text-green-900">
             Tá na época de:
           </h1>
-        </SimpleFadeIn>
 
-        <SimpleFadeIn>
           <div className="flex flex-wrap justify-between gap-2">
-            <SimpleVariant />
+            <SimpleVariant filteredFoods={filteredFoods} />
           </div>
-        </SimpleFadeIn>
 
-        <SimpleFadeIn>
-          <section>
+          <div>
             <p>
               Confira o{" "}
               <Link
@@ -30,9 +30,10 @@ export default function Home() {
               </Link>{" "}
               completo das safras
             </p>
-          </section>
-        </SimpleFadeIn>
-      </section>
+          </div>
+
+        </section>
+      </SimpleFadeIn>
     </main>
   );
 }
--- src/components/framer/SimpleVariant.tsx
+++ src/components/framer/SimpleVariant.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
-import SimpleDrag from "./SimpleDrag";
-import { fruitsAndVegetables } from "@/data/fruitsAndVegetables";
-import { filterFoodsByCurrentMonth } from "@/app/utils/utils";
 import { Food } from "@/types/types";
-
+import { FoodItem } from "../FoodItem";
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -18,26 +15,7 @@ const container = {
   },
 };
 
-const item = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1,
-  },
-};
-
-export const SimpleVariant = () => {
-  const colorVariants = {
-    red: "bg-red-400",
-    yellow: "bg-yellow-400",
-    green: "bg-green-400",
-    blue: "bg-blue-400",
-    purple: "bg-purple-400",
-    orange: "bg-orange-400",
-  };
-
-  const filteredFruitsAndVegetables = filterFoodsByCurrentMonth(fruitsAndVegetables);
-
+export const SimpleVariant = ({ filteredFoods }: { filteredFoods: Food[] }) => {  
   return (
     <motion.div
       className="flex flex-wrap justify-between gap-2"
@@ -45,16 +23,10 @@ export const SimpleVariant = () => {
       initial="hidden"
       animate="visible"
     >
-      {filteredFruitsAndVegetables.map((f: Food) => (
-        <SimpleDrag key={f.id}>
-          <motion.div
-            className={`border-gray-800 p-2 border-2 lg:border-4 lg:p-4 rounded-full ${f.tailwindBgColor}`}
-            variants={item}
-          >
-            <p className="font-semibold text-1xl lg:text-3xl">{f.name}</p>
-          </motion.div>
-        </SimpleDrag>
+      {filteredFoods.map((f: Food) => (
+        <FoodItem key={f.id} food={f} />
       ))}
     </motion.div>
   );
 };
+
