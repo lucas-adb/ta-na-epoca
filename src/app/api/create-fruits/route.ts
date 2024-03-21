5ae0c571b23dec33e624447317ed22db89ba2ab5
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-03-20 21:22:48 -0300

    feat: test vercel postgres

---
+++ src/app/api/create-fruits/route.ts
@@ -0,0 +1,16 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET(request: Request) {
+  try {
+    const result =
+      await sql`CREATE TABLE Fruits (
+        Name varchar(255),
+        Color varchar(255),
+        Type varchar(255)
+      );`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
