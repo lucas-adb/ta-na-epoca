259000fca82007bf218afcd271f6e04eb6b8d53b
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-03-22 16:04:05 -0300

    feat: add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function Custom404() {
+  return (
+    <main className="flex-grow max-w-4xl mx-auto flex flex-col sm:justify-center">
+      <section className="flex flex-col items-center gap-10 sm:gap-8 px-6 sm:px-4 mx-auto py-8">
+        <h1 className="font-serif text-6xl md:text-8xl text-green-900 text-center">
+          4(🍍)4
+        </h1>
+        <p className="text-center">Página não encontrada</p>
+      </section>
+    </main>
+  );
+}
