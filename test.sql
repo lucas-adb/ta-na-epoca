3e99aa10afe9ae5c5ea9e4cfb4381cd39e0e792c
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-03-22 15:11:34 -0300

    refactor: adds news foods to sql file

--- test.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "Food" (name, color, type)
-VALUES ("Jiló", "green", "VEGETABLE");
-
-INSERT INTO "FoodsMonths"("foodId", "monthId")
-VALUES (11, 1), (11, 2), (11, 3), (11, 4), (11, 5), (11, 6), (11, 11);
