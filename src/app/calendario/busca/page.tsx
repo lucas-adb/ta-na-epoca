a885b3179e22777e5e0fd1655bf88b693e181b0c
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-03-25 15:29:10 -0300

    style: adds padding to table

--- src/app/calendario/busca/page.tsx
+++ src/app/calendario/busca/page.tsx
@@ -10,7 +10,7 @@ const CalendarSearch = (props: CalendarSearchProps) => {
   const { food, type, month } = searchParams;
 
   return (
-    <main className="px-8 flex-grow flex flex-col sm:justify-center gap-4 sm:gap-8">
+    <main className="px-8 pb-4 flex-grow flex flex-col sm:justify-center gap-4 sm:gap-8">
       <h1 className="font-serif text-lg sm:text-4xl md:text-6xl text-green-900 text-center">
         Sazonalidade de produtos
       </h1>
--- src/app/calendario/page.tsx
+++ src/app/calendario/page.tsx
@@ -3,7 +3,7 @@ import SearchForm from "@/components/SearchForm";
 
 export default function Calendar() {
   return (
-    <main className="px-8 flex-grow flex flex-col sm:justify-center gap-4 sm:gap-8">
+    <main className="px-8 pb-4 flex-grow flex flex-col sm:justify-center gap-4 sm:gap-8">
       <h1 className="font-serif text-lg sm:text-4xl md:text-6xl text-green-900 text-center">
         Sazonalidade de produtos
       </h1>
